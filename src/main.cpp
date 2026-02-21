870506d6452ac7bca1171971334d7413d24e1d37
charvi1405 <charvi.mittal_cs.aiml24@gla.ac.in>
2026-02-21 13:50:10 +0530

    Read analog value from FSR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,7 @@ void setup() {
 }
 
 void loop() {
+    int fsrValue = analogRead(fsrPin);
 
-    // TODO 5:
-    // Read analog value from FSR
 
-    // TODO 6:
-    // Print raw ADC value
-
-    // TODO 7:
-    // Apply simple threshold logic (e.g., detect pressure)
-
-    // TODO 8:
-    // Print pressure detection message
-
-    // TODO 9:
-    // Add delay (500ms or 1 second)
 }
